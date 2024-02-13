655a4f7479be79976e3e420d4d63362b235e29bc
ivnteterin <ivnteterin@gmail.com>
2024-02-13 16:12:03 +0200

    fix footer

--- app-frontend/components/Footer.jsx
+++ app-frontend/components/Footer.jsx
@@ -1,16 +1,6 @@
 const Footer = () => (
 	<footer className='fixed h-10 bottom-0 mx-auto opacity-50'>
-		Built by&nbsp;
-		<a
-			className='italic'
-			href='https://devskills.co?utm_source=challenge-boilerplate'
-			target='_blank'
-			title='DevSkills'
-			rel='noopener noreferrer'
-		>
-			DevSkills
-		</a>
-		, task implementation by&nbsp;
+		Implemented by&nbsp;
 		<a
 			className='italic'
 			href='https://github.com/ivnteterin'
